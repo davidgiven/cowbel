650cbdc4af03401852cdeb5bec96c3873d541a59
David Given <dg@cowlark.com>
2012-01-15 23:48:32 +0000

    Dump out value of boolean constants.

--- src/com/cowlark/sake/ast/nodes/BooleanConstantNode.java
+++ src/com/cowlark/sake/ast/nodes/BooleanConstantNode.java
@@ -20,6 +20,12 @@ public class BooleanConstantNode extends ExpressionLiteralNode
 	}
 	
 	@Override
+	public String getShortDescription()
+	{
+	    return Boolean.toString(_value);
+	}
+	
+	@Override
 	public void visit(Visitor visitor) throws CompilationException
 	{
 		visitor.visit(this);
