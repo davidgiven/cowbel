3c6538d581173af75b510a2057427561847c1430
David Given <dg@cowlark.com>
2012-01-26 22:40:28 +0000

    Added copyright headers.

--- src/com/cowlark/cowbel/parser/core/MutableLocation.java
+++ src/com/cowlark/cowbel/parser/core/MutableLocation.java
@@ -1,3 +1,9 @@
+/* © 2012 David Given
+ * This file is made available under the terms of the two-clause BSD
+ * license. See the file COPYING in the distribution directory for the
+ * full license text.
+ */
+
 package com.cowlark.cowbel.parser.core;
 
 public class MutableLocation extends Location
