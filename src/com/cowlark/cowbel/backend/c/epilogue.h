d9ebb6aff42048a78918e7a8e7154be44e775a29
David Given <dg@cowlark.com>
2012-01-26 22:40:28 +0000

    Added copyright headers.

--- src/com/cowlark/cowbel/backend/c/epilogue.h
+++ src/com/cowlark/cowbel/backend/c/epilogue.h
@@ -1,3 +1,9 @@
+/* © 2012 David Given
+ * This file is made available under the terms of the two-clause BSD
+ * license. See the file COPYING in the distribution directory for the
+ * full license text.
+ */
+
 int main(int argc, const char* argv[])
 {
 	s0__main___internal__1_0();
