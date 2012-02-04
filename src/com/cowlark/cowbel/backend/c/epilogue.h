55f042fc464300dae9f83a8239bf51937afb767e
David Given <dg@cowlark.com>
2012-02-04 12:44:50 +0000

    Changed the license file; made the runtime library and test suite CC0.

--- src/com/cowlark/cowbel/backend/c/epilogue.h
+++ src/com/cowlark/cowbel/backend/c/epilogue.h
@@ -1,7 +1,15 @@
-/* © 2012 David Given
- * This file is made available under the terms of the two-clause BSD
- * license. See the file COPYING in the distribution directory for the
- * full license text.
+/* BEGIN cowbel runtime library
+ *
+ * Written in 2012 by David Given.
+ *
+ * To the extent possible under law, the author of the cowbel runtime
+ * library (of which this code, up to the string 'END cowbel runtime library',
+ * is part), has dedicated all copyright and related and neighboring rights
+ * to this software to the public domain worldwide. This software is
+ * distributed without any warranty.
+ *
+ * Please see the file COPYING.CC0 in the distribution package for more
+ * information.
  */
 
 int main(int argc, const char* argv[])
@@ -9,3 +17,5 @@ int main(int argc, const char* argv[])
 	s0__main___internal__1_0();
 	return 0;
 }
+
+/* END cowbel runtime library */
--- src/com/cowlark/cowbel/parser/parsers/ForStatementParser.java
+++ src/com/cowlark/cowbel/parser/parsers/ForStatementParser.java
@@ -1,6 +1,6 @@
 /* © 2012 David Given
  * This file is made available under the terms of the two-clause BSD
- * license. See the file COPYING in the distribution directory for the
+ * license. See the file COPYING.BSD in the distribution directory for the
  * full license text.
  */
 
